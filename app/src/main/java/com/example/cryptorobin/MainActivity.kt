f5090efe91bb08a5d18173cd8afa09c902826bd6
adityasimant <adityasimant88@gmail.com>
2022-11-04 16:20:03 +0530

    Bottom navigation

--- app/src/main/java/com/example/cryptorobin/MainActivity.kt
+++ app/src/main/java/com/example/cryptorobin/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.cryptorobin
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
