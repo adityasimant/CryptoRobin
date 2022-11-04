2852b684715c2c87a2144fa53db3f7123c5e87dd
adityasimant <adityasimant88@gmail.com>
2022-11-04 17:33:02 +0530

    Fragment manager and fragment changing

--- app/src/main/java/com/example/cryptorobin/MainActivity.kt
+++ app/src/main/java/com/example/cryptorobin/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.cryptorobin
 
 import android.os.Bundle
+import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.navigation.fragment.findNavController
+import com.example.cryptorobin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentcontainer)
+        val navController = navHostFragment!!.findNavController()
+
+        val popupMenu = PopupMenu(this,null)
+        popupMenu.inflate(R.menu.bottom_nav)
+        binding.bottomBar.setupWithNavController(popupMenu.menu, navController)
+
+
+
     }
+
 }
