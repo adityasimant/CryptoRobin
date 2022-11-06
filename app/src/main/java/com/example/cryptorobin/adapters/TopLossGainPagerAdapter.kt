92a4a9c9009363e489c2fd006b8c34b7dd298c02
adityasimant <adityasimant88@gmail.com>
2022-11-06 17:48:35 +0530

    Adapter for homeFragment viewpager

---
+++ app/src/main/java/com/example/cryptorobin/adapters/TopLossGainPagerAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.cryptorobin.adapters
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.cryptorobin.Fragments.TopLossGainFragment
+
+class TopLossGainPagerAdapter(fragment : Fragment) : FragmentStateAdapter(fragment){
+    override fun getItemCount(): Int {
+        return 2
+    }
+
+    override fun createFragment(position: Int): Fragment {
+
+        val fragment = TopLossGainFragment()
+        val bundle = Bundle()
+        bundle.putInt("position", position)
+        fragment.arguments = bundle
+        return fragment
+    }
+
+
+}
